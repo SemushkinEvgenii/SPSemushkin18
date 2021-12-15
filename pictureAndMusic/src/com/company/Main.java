a6ebdb0346b235d8afea0b0179299d2eca42c8cb
Evgenii <77569807+SemushkinEvgenii@users.noreply.github.com>
2021-12-15 18:19:59 +0300

    pictureAndMusic

---
+++ pictureAndMusic/src/com/company/Main.java
@@ -0,0 +1,7 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        //Создание потока и присваивание приоритета
+        ThreadLoad turtle = new ThreadLoad("Музыка");
+        ThreadLoad rabbit = new ThreadLoad("Картинка");
+    }}
